8c8158ce36f642a046ef125b9aab787622a5f778
Dhiamissaoui <dhiamissaoui17@gmail.com>
2025-09-30 00:24:58 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Nav from './components/navbar';
+import CardComponent from './components/cards';
+
+
+function App() {
+  return (
+    <>
+      <div className='App'>
+        <Nav />
+        <CardComponent />
+      </div>
+    </>
+  );
+}
+
+export default App;
